7a258896b7467b3ae7cbc4add190823e87212b42
Prasad <prasadchelsea33@gmail.com>
2018-05-15 18:59:56 +0530

    Completed training module , added bias weight, increased number of points for better visualization, updated readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # NeuralNetworkJS
 Neural Networks in Javascript using p5.js
+
+#### Perceptrons Outline:
+1. Provide the Perceptrons inputs for which the answer is known
+2. Ask Perceptrons to guess the answer
+3. Calculate the error
+4. Adjust the weights according to the error so as to steer the guessed answer towards the actual answer
+5. Go to Step 1 and repeat
+
+
+**Equations**<br>
+<a href="https://www.codecogs.com/eqnedit.php?latex=\Delta&space;Weight&space;=&space;Error&space;*&space;Input" target="_blank"><img src="https://latex.codecogs.com/gif.latex?\Delta&space;Weight&space;=&space;Error&space;*&space;Input" title="\Delta Weight = Error * Input" /></a>
+<br/>
+<a href="https://www.codecogs.com/eqnedit.php?latex=New&space;Weight&space;=&space;Weight&space;&plus;&space;\Delta&space;Weight" target="_blank"><img src="https://latex.codecogs.com/gif.latex?New&space;Weight&space;=&space;Weight&space;&plus;&space;\Delta&space;Weight" title="New Weight = Weight + \Delta Weight" /></a>
--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-// Using p5.js
+// Simple Perceptrons Using p5.js
+
+const LEARNING_RATE = 0.1; // keeping it low for interesting and long lasting visualization
+const NUMBER_OF_WEIGHTS = 3; // x,y and bias
+const NUMBER_OF_POINTS = 1000; // number of points to train
 
 function Perceptrons()
 {
   this.weights = randomWeights();
-  //document.write(this.weights);
 
   this.guess = function(inputs)
   {
@@ -13,13 +16,13 @@ function Perceptrons()
       sum += this.weights[j] * inputs[j];
     }
 
-    var out = sign(sum);
+    var out = activation(sum);
     return out;
   }
 
 
   // Activation function
-  function sign(number)
+  function activation(number)
   {
     if(number >= 0)
       return 1;
@@ -28,26 +31,41 @@ function Perceptrons()
     }
   }
 
+  // trains a point
+  this.train = function(inputs,target)
+  {
+    var guess = this.guess(inputs);
+    var error = target - guess;
+
+    // adjusting weights
+    for(var p=0;p<this.weights.length;p++)
+    {
+      this.weights[p] += error * inputs[p] * LEARNING_RATE;
+    }
+  }
 
 }
 
+// initialize with random weights
 function randomWeights()
 {
-  var weights = new Array(2);
+  var weights = new Array(NUMBER_OF_WEIGHTS);
   // Assigning random weights
   for(var i=0;i<weights.length;i++)
   {
-    // returns number between -1 and 1 (inclusive)
+    // returns integers between -1 and 1 (inclusive)
     weights[i] = Math.round((Math.random() * (1-(-1))) - 1);
   }
 
   return weights;
 }
 
+// subjects for training
 function Point()
 {
   this.x = Math.floor(Math.random() * width);
   this.y = Math.floor(Math.random() * height);
+  this.z = 1; // bias
   this.label = function()
   {
     if(this.x > this.y)
@@ -56,41 +74,104 @@ function Point()
       return -1;
   }
 
+  // display the points
   this.show = function()
   {
     if(this.label() == 1)
-      fill(0,255,0);
+      fill(255);
     else {
-      fill(255,0,0);
+      fill(0);
     }
     ellipse(this.x,this.y,10,10);
   }
 }
 
-var points = new Array(100);
+var points = new Array(NUMBER_OF_POINTS);
+var perceptronBrain;
 
-// Main
 function setup(){
   createCanvas(600,600);
-  var p1 = new Perceptrons();
+  background(135,206,250);
 
+  perceptronBrain = new Perceptrons();
+
+  // create points
   for(var i = 0;i<points.length;i++)
   {
     points[i] = new Point();
   }
-  // document.write(Math.floor(Math.random() * width));
-  // document.write("<br/>");
-  // document.write(Math.floor(Math.random() * height));
 }
+
+var trainIndex = 0;
 
 function draw()
 {
-  background(135,206,250);
-
   // dividing line
   line(0,0,width,height);
+
+  // draws the original points
+  drawPoints(points);
+
+  drawTrainedPoints(points);
+
+  // Trains one point at a time
+  trainPointByPoint(points,trainIndex);
+
+  trainIndex++;
+
+  // Once it has trained all the points we start again
+  if(trainIndex == points.length)
+  { trainIndex = 0;
+  }
+}
+
+function drawPoints(points)
+{
   for(var x = 0;x < points.length;x++)
   {
     points[x].show();
   }
 }
+
+function drawTrainedPoints(points)
+{
+  for(var k = 0;k < points.length;k++)
+  {
+    var currentPoint = points[k];
+    var inputs = [currentPoint.x,currentPoint.y,currentPoint.z];
+    var target = currentPoint.label();
+
+    var guess = perceptronBrain.guess(inputs);
+    if(guess == target)
+    {
+      fill(0,255,0);
+    }
+    else {
+      fill(255,0,0);
+    }
+    noStroke();
+    ellipse(currentPoint.x,currentPoint.y,5,5);
+  }
+}
+
+
+function trainPointByPoint(points,trainIndex)
+{
+  var trainPoint = points[trainIndex];
+  var inputs = [trainPoint.x,trainPoint.y,trainPoint.z];
+  var target = trainPoint.label();
+  perceptronBrain.train(inputs,target);
+}
+
+
+// train points using MousePress (extra)
+// function mousePressed()
+// {
+//   for(var k = 0;k < points.length;k++)
+//   {
+//     var currentPoint = points[k];
+//     var inputs = [currentPoint.x,currentPoint.y,currentPoint.z];
+//     var target = currentPoint.label();
+//     perceptronBrain.train(inputs, target);
+//   }
+// }
